b0179e0ccdf2f53e2ee012715e9a798303254767
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2024-01-18 12:03:45 +0100

    Removed astro

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import vituum from 'vituum'
+import tailwindcss from '@vituum/vite-plugin-tailwindcss'
+import posthtml from '@vituum/vite-plugin-posthtml'
+
+export default {
+    plugins: [
+        vituum(),
+        posthtml({
+            root: './src'
+        }),
+        tailwindcss()
+    ]
+}
