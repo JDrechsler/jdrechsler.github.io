b10df2b55ff315440969f37855184946d537118a
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2024-01-31 13:21:50 +0100

    Added config for public and docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
 import vituum from 'vituum'
 import tailwindcss from '@vituum/vite-plugin-tailwindcss'
 import posthtml from '@vituum/vite-plugin-posthtml'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default {
+  base: '/jdrechsler.github.io/',
+  build: {
+    assetsDir: 'media',
+    copyPublicDir: false,
+    outDir: 'docs',
+  },
   plugins: [
     vituum(),
     posthtml({
       root: './src',
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public',
+          dest: '',
+        },
+      ],
     }),
     tailwindcss(),
   ],
