16c25b7c5defdfb83e5f2436af85aba5ab129606
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2024-01-31 13:26:21 +0100

    Dev -> main (#1)
    * Removed astro
    * Added styles for Navbar and socials
    * Added socials to navbar
    * Added Hero, Footer, WorkingTogether
    * Added nicer theme
    * Added config for public and docs folder

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./src/**/*.{js,html}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '800px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        accent: '#96b0bb',
+      },
+    },
+  },
+}
