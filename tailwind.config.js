58ac61f56829ada33ca12eba4d1223356a5ca989
Johannes Drechsler <johannesdrechslerus@gmail.com>
2024-02-07 12:40:33 +0100

    Moved to astro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 export default {
-  content: ['./src/**/*.{js,html}'],
-  theme: {
-    screens: {
-      sm: '480px',
-      md: '800px',
-      lg: '1024px',
-      xl: '1440px',
-    },
-    extend: {
-      colors: {
-        accent: '#96b0bb',
+    content: ['./src/**/*.{js,html,astro}'],
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '800px',
+        lg: '1024px',
+        xl: '1440px',
+      },
+      extend: {
+        colors: {
+          accent: '#96b0bb',
+        },
       },
     },
-  },
-}
+  }
+  
