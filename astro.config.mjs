124c78a4c46aaf402de8757c83c5f68550496295
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2024-02-09 19:18:56 +0100

    Added more content to about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jdrechsler.com',
-  integrations: [tailwind()]
+  site: "https://jdrechsler.com",
+  integrations: [tailwind()],
+  devToolbar: {
+    enabled: false,
+  },
 });
